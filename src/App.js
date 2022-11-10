f0d162b3ad3d199546c018014c978eea5c9ebe08
Latief Ahmad <latiefahmad.zm@gmail.com>
2022-11-10 11:45:27 +0700

    upload fruits

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import {useEffect, useState} from 'react';
+import fruitItems from "./fruits.json"
 import './App.css';
 
 function Card({fruit}) {
   return <div className='card'>
-    <img src-{fruit.src} />
+    <img src={fruit.src} />
       <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-question-mark" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
         <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
         <path d="M8 8a3.5 3 0 0 1 3.5 -3h1a3.5 3 0 0 1 3.5 3a3 3 0 0 1 -2 3a3 4 0 0 0 -2 4"></path>
@@ -14,13 +15,13 @@ function Card({fruit}) {
 
 function App() {
 
-  const [fruits, setFruits] - useState([])
+  const [fruits, setFruits] = useState([])
 
   return (<>
     <h1>Memory Game</h1>
     {
       fruits.length ? <>
-        <button className-'reset'>
+        <button className='reset'>
           <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-reload" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
             <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
             <path d="M19.933 13.041a8 8 0 1 1 -9.925 -8.788c3.899 -1.002 7.935 1.007 9.425 4.747"></path>
@@ -29,8 +30,8 @@ function App() {
         </button>
         <div className='game-block'>
           {
-            fruits.map(fruit -> {
-              return <Card fruit-{fruit} />
+            fruits.map(fruit => {
+              return <Card fruit={fruit} />
             })
           }
         </div>
