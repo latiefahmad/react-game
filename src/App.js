4a523e5a009945d0ec845f3901ad0e607d14a964
DC-Kantor <latief@uniramalang.ac.id>
2025-07-15 18:13:03 +0700

    update responsive

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ function Card({fruit, flipped, chooseCard }) {
   };
 
   return <div className={`card ${flipped ? 'matched' : ''}`} onClick={cardClickHandle}>
-    <img style={{transform:'rotateY(180deg)'}} src={fruit.src} />
-      <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-question-mark" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+    <img style={{transform:'rotateY(180deg)'}} src={fruit.src} alt={fruit.name || 'Memory card'} />
+      <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-question-mark" width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
         <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
         <path d="M8 8a3.5 3 0 0 1 3.5 -3h1a3.5 3 0 0 1 3.5 3a3 3 0 0 1 -2 3a3 4 0 0 0 -2 4"></path>
         <line x1="12" y1="19" x2="12" y2="19.01"></line>
@@ -81,7 +81,7 @@ function App() {
     {
       fruits.length ? <>
         <button onClick={resetGame} className='reset'>
-          <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-reload" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+          <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-reload" width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
             <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
             <path d="M19.933 13.041a8 8 0 1 1 -9.925 -8.788c3.899 -1.002 7.935 1.007 9.425 4.747"></path>
             <path d="M20 4v5h-5"></path>
@@ -101,6 +101,11 @@ function App() {
         </div>
       </> : <button onClick={initGame} className='start-game'>Start Game</button>
     }
+    <div className='copyright-footer'>
+      <div className='circle'></div>
+      <span>© 2025 ltfhmd</span>
+      <div className='circle'></div>
+    </div>
   </>
   );
 }
